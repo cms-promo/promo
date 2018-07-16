ecd4797ef6718affd4a69af6614e2df67c3c8e4b
Fox Chao <fox@steamcomputing.com>
2018-07-17 07:34:43 +0800

    add javascript dependency

---
+++ app/javascript/index.js
@@ -0,0 +1,20 @@
+import $ from 'jquery'
+global.$ = $
+global.jQuery = $
+
+import 'bootstrap'
+import '../stylesheets/vendor.scss'
+
+import Rails from 'rails-ujs'
+global.Rails = Rails
+
+import Turbolinks from 'turbolinks'
+global.Turbolinks = Turbolinks
+
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+import 'animate.css'
+import '@fortawesome/fontawesome-free/css/all.css';
+import 'simple-line-icons/scss/simple-line-icons.scss'
+
