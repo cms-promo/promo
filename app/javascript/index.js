f108d8b35c3b1a5bf3785e4b6c1e84d4a5070909
Fox Chao <fox@steamcomputing.com>
2018-07-24 11:28:11 +0800

    try to make font-awesome compatible with han

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -2,20 +2,19 @@ import $ from 'jquery'
 global.$ = $
 global.jQuery = $
 
-import 'bootstrap'
-import './stylesheets/vendor.scss'
-
 import Rails from 'rails-ujs'
 global.Rails = Rails
 
 import Turbolinks from 'turbolinks'
 global.Turbolinks = Turbolinks
 
+import 'bootstrap'
+import './stylesheets/vendor.scss'
+
 import AOS from 'aos'
 global.AOS = AOS
 
 import 'aos/dist/aos.css'
-
 import 'animate.css'
-import '@fortawesome/fontawesome-free/css/all.css';
+import 'font-awesome/scss/font-awesome-han.scss';
 import 'simple-line-icons/scss/simple-line-icons.scss'
