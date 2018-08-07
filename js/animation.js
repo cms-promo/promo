30c4aa7e833901aaaf599984bdcb4b33470bc2ef
Fox Chao <fox@steamcomputing.com>
2018-08-07 08:06:22 +0800

    try to integrate animate.css, still fail

--- js/animation.js
+++ js/animation.js
@@ -1,3 +1,25 @@
 import 'aos/dist/aos.css'
 import AOS from 'aos'
 global.AOS = AOS
+
+/*
+//COPY EITHER OF THE FOLLOWING CODE SNIPPETS TO SUPPORT NATIVE OR ANIMATE.CSS ANIMATEION
+
+AOS.init({
+  startEvent: 'turbolinks:load',    // name of the event dispatched on the document, that AOS should initialize on
+})
+
+------------------------------------------------------------------------------------------------------------------
+
+//FIXME 
+import 'animate.css'
+
+import 'promo/css/animated-aos.css'
+AOS.init({
+  // Global settings
+  startEvent: 'turbolinks:load',    // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: null,              // class applied after initialization
+  animatedClassName: 'animated',    // class applied on animation
+  useClassNames: true,              // if true, will add content of `data-aos` as classes on scroll
+})
+*/
