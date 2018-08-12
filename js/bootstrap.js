024d6a830872596675f48a70af9a5bb2e619fe5e
Fox Chao <fox@steamcomputing.com>
2018-08-12 19:59:23 +0800

    add bootstrap-combobox

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,2 +1,4 @@
 import '../css/vendor.scss'
 import 'bootstrap'
+import '@danielfarrell/bootstrap-combobox/css/bootstrap-combobox.css'
+import '@danielfarrell/bootstrap-combobox/js/bootstrap-combobox.js'
