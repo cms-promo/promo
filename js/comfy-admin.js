d7b579171c39a6243fdfd8b899921529ffba9fde
Fox Chao <fox@steamcomputing.com>
2018-08-15 20:33:03 +0800

    auto expand the width

--- js/comfy-admin.js
+++ js/comfy-admin.js
@@ -1,2 +1,12 @@
 import 'jquery-table-fixed-head'
 import '../css/jquery-table-fixed-head.scss'
+
+//document.addEventListener('ready turbolinks:load', () => {
+(() => {
+  if (document.location.pathname.indexOf("/admin/") === 0) {
+    $( "#cms-right" ).remove()
+    $( "#cms-main" ).removeClass( "col-lg-8 m-auto" )     // remove all old classes
+    $( "#cms-main" ).addClass( "col-lg-10 offset-lg-2" )  // expand width and fix offset
+  }
+})();
+//});
